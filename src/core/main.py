a0f467d4863c0a4f5af79d2c991a859393fba2c6
briang0 <brianguidarini@gmail.com>
2019-06-13 00:02:12 -0500

    Added functionality to analyzation

--- src/core/main.py
+++ src/core/main.py
@@ -1,22 +1,26 @@
+#This class is for testing. It won't be a part of the api
+
 import os, sys
 sys.path.insert(0, os.path.abspath("../.."))
 import src.io.audio_reader as read
 import src.signal_processing.fft as fft
+import src.signal_processing.analyzation as anlyz
 import src.util.format as fmt
 import src.util.mapping as mapping
 
 path = "C:/Users/brian/Documents/Smasheo/Audio/King Dedede Sounds/dededehit.wav"
 
+fft_size = 128
+
 data = read.wav_to_vec(path)
 
-mat = fmt.vec_to_mat(data, 128)
+mat = fmt.vec_to_mat(data, fft_size)
 
 cmplx_mat = mapping.real_to_cmplx_obj_mat(mat)
 
 fft_mat = fft.fft_mat(cmplx_mat)
 
-# for row in fft_mat:
-#     for elem in row:
-#         print(elem)
+for row in fft_mat:
+    print(anlyz.get_max_intensity_cmplx_vec(row), anlyz.get_min_intensity_cmplx_vec(row) )
 
 # transform = fft.fft(data, 256)
--- src/signal_processing/analyzation.py
+++ src/signal_processing/analyzation.py
@@ -1,8 +1,38 @@
-from math import Complex as cmplx
+import os, sys
+sys.path.insert(0, os.path.abspath("../.."))
+import src.math.Complex as cmplx
+import numpy as np
 
-def getIntensity(complex, FFTSize):
+max16bit = 32768
+
+def get_magnitude(complex):
     real = complex.real
     imag = complex.imag
-    magnitude = np.sqrt(real * real + imag * imag)
-    intensity = -20 * np.log10(magnitude/max16bit)
-    return intensity
+    return np.sqrt(real * real + imag * imag)
+
+def get_intensity(complex, FFTSize):
+    magnitude = get_magnitude(complex)
+    intensity = 20 * np.log10(magnitude/(max16bit))
+    return np.abs(intensity)
+
+def bin_to_hz(bin, fft_size, sample_rate):
+    step = sample_rate / fft_size
+    return bin * step
+
+def get_max_intensity_cmplx_vec(vec):
+    fft_size = len(vec)
+    max = get_intensity(vec[0], fft_size)
+    for num in vec:
+        cur_intensity = get_intensity(num, fft_size)
+        if (cur_intensity > max):
+            max = cur_intensity
+    return max
+
+def get_min_intensity_cmplx_vec(vec):
+    fft_size = len(vec)
+    min = get_intensity(vec[0], fft_size)
+    for num in vec:
+        cur_intensity = get_intensity(num, fft_size)
+        if (cur_intensity < min):
+            min = cur_intensity
+    return min
